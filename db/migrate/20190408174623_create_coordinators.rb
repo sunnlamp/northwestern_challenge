388c95948e0fc5c6f9797a9180b07e5dcaab8fd8
Rogelio Zamudio <yeyozaz@gmail.com>
2019-04-08 12:46:51 -0500

    added coordinator scaffold to project

---
+++ db/migrate/20190408174623_create_coordinators.rb
@@ -0,0 +1,11 @@
+class CreateCoordinators < ActiveRecord::Migration[5.2]
+  def change
+    create_table :coordinators do |t|
+      t.string :email
+      t.string :name
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
